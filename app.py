2a048d6c9eb01ecfc23686032fadaae59d9352d7
jas003ucsd <jas003@ucsd.edu>
2022-03-30 21:02:05 -0700

    added requirements.txt

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response, FileResponse
+import os
 # -------------------------Server----------------------------------
 
 def get_home(req):
@@ -16,7 +17,7 @@ if __name__ == '__main__':  # Program entrance
         config.add_static_view(name='/', path='./public', cache_max_age=3600)
 
         app = config.make_wsgi_app()
-
-    server = make_server('0.0.0.0', 6544, app)
-    print('Web server started on: http://0.0.0.0:6544')
+    port = int(os.environ.get("PORT", 5000))
+    server = make_server('0.0.0.0', port, app)
+    # print('Web server started on: http://0.0.0.0:6544')
     server.serve_forever()
