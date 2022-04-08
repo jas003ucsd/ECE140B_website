c4f7d6ce9e19a55549303cbf60e6d5f21a5e2640
jas003ucsd <jas003@ucsd.edu>
2022-04-08 15:55:16 -0700

    restructure files

--- web/src/server.py
+++ web/src/server.py
@@ -11,14 +11,21 @@ db_name = os.environ['MYSQL_DATABASE']
 db_host = os.environ['MYSQL_HOST']
 
 def get_home(req):
-    return FileResponse("home.html")
+  # Connect to the database and retrieve the users
+  db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
+  cursor = db.cursor()
+  cursor.execute("select first_name, last_name, email from Users;")
+  records = cursor.fetchall()
+  db.close()
+
+  return render_to_response('templates/home.html', {'users': records}, request=req)
 
 ''' Route Configurations '''
 if __name__ == '__main__':
   config = Configurator()
 
-  # config.include('pyramid_jinja2')
-  # config.add_jinja2_renderer('.html')
+  config.include('pyramid_jinja2')
+  config.add_jinja2_renderer('.html')
 
   config.add_route('get_home', '/')
   config.add_view(get_home, route_name='get_home')
