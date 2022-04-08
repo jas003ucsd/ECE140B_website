599196c506f6c6d3b2ad76f568f7195163c9c547
jas003ucsd <jas003@ucsd.edu>
2022-04-07 21:28:17 -0700

    added requirements.txt

---
+++ web/src/app.py
@@ -0,0 +1,21 @@
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response, FileResponse
+import os
+# -------------------------Server----------------------------------
+
+def get_home(req):
+    return FileResponse("index.html")
+
+if __name__ == '__main__':  # Program entrance
+
+    config = Configurator()
+
+    config.add_route('home', '/')
+    config.add_view(get_home, route_name='home')
+
+    config.add_static_view(name='/', path='./public', cache_max_age=3600)
+
+    app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', 6000, app)
+    server.serve_forever()
